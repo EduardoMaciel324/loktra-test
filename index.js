5a313db57a1643f67eb77f6bbe843937e44b3d96
Eduardo Maciel <mrgamex49@gmail.com>
2021-04-21 16:55:48 -0300

    refactor: aplly correct BEM

--- index.js
+++ index.js
@@ -1,56 +1,64 @@
 window.onload = () => {
-	startProgressBar();
-	makeClickGiveNow();
-	makeInputMask();
-	shareMessageOfTwitterAndFacebook();
-	saveDonationForLater();
+  cancelReloadOfForm();
+  startProgressBar();
+  makeClickGiveNow();
+  makeInputMask();
+  shareMessageOfTwitterAndFacebook();
+  saveDonationForLater();
 };
+function cancelReloadOfForm() {
+  const form = document.querySelector("form");
+  form.addEventListener("submit", (event) => event.preventDefault());
+}
 function animateOfProgressBar(from, time = 20) {
-	var elem = document.getElementById("donation__progress--value");
-	var width = elem.style.width.replace("%", "");
-	var id = setInterval(frame, time);
-	function frame() {
-		if (width == from || width >= 100) {
-			clearInterval(id);
-			return;
-		}
-		width++;
-		elem.style.width = width + "%";
-	}
+  const elem = document.getElementById("donation__progress--value");
+  let width = elem.style.width.replace("%", "");
+  var id = setInterval(frame, time);
+  function frame() {
+    if (width == from || width >= 100) {
+      clearInterval(id);
+      return;
+    }
+    width++;
+    elem.style.width = width + "%";
+  }
 }
 function startProgressBar() {
-	animateOfProgressBar(68);
+  animateOfProgressBar(68);
 }
 
 function makeInputMask() {
-	var element = document.getElementById("donation__input-value");
-	var mask = IMask(element, {
-		mask: "${000}",
-	});
-	return;
+  const element = document.getElementById("donation__input-value");
+  IMask(element, {
+    mask: "${000}",
+  });
 }
 // Can do % of input and total
 function makeClickGiveNow() {
-	var element = document.getElementById("donation__button-givenow");
-	element.onclick = function () {
-		animateOfProgressBar(100, 40);
-	};
+  const element = document.getElementById("donation__button-givenow");
+  element.onclick = function () {
+    animateOfProgressBar(100, 40);
+  };
 }
 
 function shareMessageOfTwitterAndFacebook() {
-	var element = document.getElementById("donation__footer-button-tellfriends");
-	element.onclick = () =>
-		showMessage("Shared Yay, I donated! on Facebook and Twitter.", "3000");
+  const element = document.getElementById(
+    "donation__footer-button-tellfriends"
+  );
+  element.onclick = () =>
+    showMessage("Shared Yay, I donated! on Facebook and Twitter.", "3000");
 }
 
 function saveDonationForLater() {
-	var element = document.getElementById("donation__footer-button-saveforlater");
-	element.onclick = () => showMessage("Donation saved!", "3000");
+  const element = document.getElementById(
+    "donation__footer-button-saveforlater"
+  );
+  element.onclick = () => showMessage("Donation saved!", "3000");
 }
 
 function showMessage(text, time) {
-	Toastify({
-		text: text,
-		duration: time,
-	}).showToast();
+  Toastify({
+    text: text,
+    duration: time,
+  }).showToast();
 }
